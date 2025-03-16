42db5488eedead96fdd9bf742a43169de990c65d
preeti <pp5716298@gmail.com>
2025-03-16 19:39:04 +0530

    okay

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const referralRoutes = require("./routes/referralRoutes");
+const referralRoutes = require("./src/routes/referralRoutes");
 
 const app = express();
 app.use(cors());
